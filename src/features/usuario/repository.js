450252ace400e46c069d9ec05ca6964816065b0a
GABRIEL AMORIM SANTOS <gabriel.santos2274@etec.local>
2026-09-08 10:39:05 -0300

    Login working

--- src/features/usuario/repository.js
+++ src/features/usuario/repository.js
@@ -49,12 +49,16 @@ const UsuarioRepository = {
     return result;
   },
 
-  async login(email) {
+    async login(email) {
     const [usuario] = await pool.execute(
-      `SELECT id, funcionario, email, senha, nivel_acesso, ativo FROM usuario`, [email]
-    )
-    return usuario[0];
-  },
+    `SELECT id, funcionario, email, senha, nivel_acesso, ativo
+     FROM usuario
+     WHERE email = ?`,
+    [email]
+  );
+
+  return usuario[0];
+},
 
   async getById(id) {
     const [usuario] = await pool.execute(
